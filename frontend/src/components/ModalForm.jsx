f96e801cc6b1285992b9c100f55dfbbc51fbf4e5
jay0425 <heedev94@gmail.com>
2025-03-07 23:57:53 +0900

    모달에 input 및 select 추가

--- frontend/src/components/ModalForm.jsx
+++ frontend/src/components/ModalForm.jsx
@@ -6,7 +6,30 @@ export default function ModalForm({ isOpen, onClose, mode, OnSubmit }) {
         <div className="modal-box">
           <h3 className="font-bold text-lg py-4">{mode === 'edit' ? 'Edit Client' : 'Client Details'}</h3>
           <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
+            <label className="input">
+              Name
+              <input type="text" className="grow" placeholder="" />
+            </label>
+            <label className="input">
+              Email
+              <input type="text" className="grow" placeholder="" />
+            </label>
+            <label className="input">
+              Job
+              <input type="text" className="grow" placeholder="" />
+            </label>
+
+            <div className="flex mb-4 justify-between">
+              <label className="input">
+                Rate
+                <input type="number" className="grow" placeholder="" />
+              </label>
+              <select id="active" className="select select-bordered w-full max-w-xs">
+                <option>Inactive</option>
+                <option>Active</option>
+              </select>
+            </div>
+
             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={onClose}>
               ✕
             </button>
